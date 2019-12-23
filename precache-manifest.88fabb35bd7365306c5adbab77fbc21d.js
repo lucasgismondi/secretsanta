82b7b1470f5964150905ab3fd7816865e315b938
john doe <johndoe@johns-MacBook-Pro.local>
2019-12-22 20:08:06 -0500

    Updates

---
+++ precache-manifest.88fabb35bd7365306c5adbab77fbc21d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44721d339dc17d7b2588849976789832",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f473c303d8f406f168b",
+    "url": "/static/css/main.45154457.chunk.css"
+  },
+  {
+    "revision": "b4828c0b21472b4e1817",
+    "url": "/static/js/2.c858afd3.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.c858afd3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f473c303d8f406f168b",
+    "url": "/static/js/main.0ebb058b.chunk.js"
+  },
+  {
+    "revision": "149642c2b69f86080224",
+    "url": "/static/js/runtime-main.c0fd5858.js"
+  },
+  {
+    "revision": "1b5029d8dfb20c2697d21148c2364d6e",
+    "url": "/static/media/santa2.1b5029d8.png"
+  }
+]);
